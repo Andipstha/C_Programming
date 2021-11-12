7fee6784f0a42645cc3181c6f3f3377d3de6e953
anjanshrestha8 <anjanshrestha2002@gmail.com>
2021-11-13 02:19:52 +0545

    initial commit

--- Pattern/Pattern_6.c
+++ Pattern/Pattern_6.c
@@ -10,7 +10,7 @@ int main()
     int i=0,j=0,k=0;
     for(i=1;i<=5;i++){
         for(k=i;k<=5;k++){
-            printf("  ");
+            printf(" ");
         }
         for(j=1;j<=2*i-1;j++){
             printf("* ");
